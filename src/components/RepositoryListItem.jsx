803f5046759f755015ca66ebdd5a552a174aa0b7
AntonioJesusRG <antoniogar129@gmail.com>
2023-07-09 00:55:27 +0200

    Parse big numbers

--- src/components/RepositoryListItem.jsx
+++ src/components/RepositoryListItem.jsx
@@ -9,16 +9,20 @@ const styles = StyleSheet.create({
 	},
 });
 
+const parseThousands = (value) => {
+	return value >= 1000 ? `${Math.round(value / 100) / 10}k` : String(value);
+};
+
 const RepositoryStats = (props) => {
 	return (
 		<View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
 			<View>
 				<StyledText fontWeight='bold'>Starts</StyledText>
-				<StyledText>{props.stargazersCount}</StyledText>
+				<StyledText>{parseThousands(props.stargazersCount)}</StyledText>
 			</View>
 			<View>
 				<StyledText fontWeight='bold'>Forks</StyledText>
-				<StyledText>{props.forksCount}</StyledText>
+				<StyledText>{parseThousands(props.forksCount)}</StyledText>
 			</View>
 			<View>
 				<StyledText fontWeight='bold'>Review</StyledText>
