bd316994709095c15a3ca034c39aff38ed7cdeec
AntonioJesusRG <antoniogar129@gmail.com>
2023-07-08 23:46:31 +0200

    Include list

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import RepositoryList from './RepositoryList.jsx';
+
+const Main = () => {
+	return (
+		<View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
+			<Text>Rate Repository List Application</Text>
+			<RepositoryList></RepositoryList>
+		</View>
+	);
+};
+
+export default Main;
