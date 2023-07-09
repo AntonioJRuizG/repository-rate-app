3c8c87716624e6e9fa68858a099fa2281426b389
AntonioJesusRG <antoniogar129@gmail.com>
2023-07-09 18:50:27 +0200

    Wrap app with native router

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
-import { View, Text } from 'react-native';
+import { NativeRouter } from 'react-router-native';
+
 import Main from './src/components/Main';
 
 export default function App() {
   return (
-    <Main></Main>
+    <NativeRouter> 
+      <Main></Main>
+    </NativeRouter>
   );
 }
