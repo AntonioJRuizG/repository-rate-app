f51fc0764a876f895de9a430f92507b8ba4d3c00
AntonioJesusRG <antoniogar129@gmail.com>
2023-07-10 00:10:45 +0200

    Fetch data from api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList } from 'react-native';
-import repositories from '../../data/repositories';
 import RepositoryListItem from './RepositoryListItem';
 
 const RepositoryList = () => {
+	const [repositories, setRepositories] = useState(null);
+
+	const fetchRepositories = async () => {
+		const response = await global
+			.fetch('https://rate-repository-api-nng9.onrender.com/api/repositories')
+			.catch((error) => {
+				console.log(error);
+				return;
+			});
+
+		if (response === undefined) return;
+
+		const data = await response.json().catch((error) => {
+			console.log(error);
+		});
+
+		setRepositories(data);
+	};
+
+	useEffect(() => {
+		fetchRepositories();
+		setRepositories(repositoriesNodes);
+	}, []);
+
+	const repositoriesNodes = repositories
+		? repositories?.edges?.map((edge) => edge.node)
+		: [];
+
 	return (
 		<FlatList
-			data={repositories}
+			data={repositoriesNodes}
 			ItemSeparatorComponent={() => <Text> </Text>}
 			renderItem={({ item: repo }) => (
 				<RepositoryListItem {...repo}></RepositoryListItem>
